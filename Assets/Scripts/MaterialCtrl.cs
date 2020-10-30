dfd562cc9a06a1ff05f83c3e177ff88dfe1234ac
macbook <mac@dreamo>
2020-10-30 23:59:56 +0900

    Lesson2Completed

--- Assets/Scripts/MaterialCtrl.cs
+++ Assets/Scripts/MaterialCtrl.cs
@@ -4,18 +4,25 @@ using UnityEngine;
 
 public class MaterialCtrl : MonoBehaviour
 {
-    [SerializeField]
-    MeshRenderer planeMeshRender;
+    
+    private Material myMaterial;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        MeshRenderer myMeshRender = gameObject.GetComponent<MeshRenderer>();
+        myMaterial = myMeshRender.material;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 myPosition = transform.position;
+        float red   = Mathf.Clamp(Mathf.Abs(myPosition.x), 0.0f, 1.0f);
+        float green = Mathf.Clamp(Mathf.Abs(myPosition.y), 0.0f, 1.0f);
+        float blue  = Mathf.Clamp(Mathf.Abs(myPosition.z), 0.0f, 1.0f);
+        myMaterial.SetFloat("_MyR", red);
+        myMaterial.SetFloat("_MyG", green);
+        myMaterial.SetFloat("_MyB", blue);
     }
 }
